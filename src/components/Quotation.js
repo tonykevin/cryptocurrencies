1c0215f889f1deaa35db41b31ff8a9c6aa30a987
tonykevin <hi@tonykevin.com>
2020-02-19 20:28:51 -0500

    add quotation styles

--- src/components/Quotation.js
+++ src/components/Quotation.js
@@ -1,16 +1,38 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const QuotationContainer = styled.div`
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+const Info = styled.p`
+  font-size: 18px;
+
+  span {
+    font-weight: bold;
+  }
+`
+
+const Price = styled.p`
+  font-size: 30px;
+
+  span {
+    font-weight: bold;
+  }
+`
 
 const Quotation = ({ result }) => {
   if (Object.keys(result).length === 0) return null
 
   return (
-    <div>
-      <p>El precio es: <span>{result.PRICE}</span></p>
-      <p>El precio más alto del día: <span>{result.HIGHDAY}</span></p>
-      <p>El precio más bajo del día: <span>{result.LOWDAY}</span></p>
-      <p>Variación últimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></p>
-      <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
-    </div>
+    <QuotationContainer>
+      <Price>El precio es: <span>{result.PRICE}</span></Price>
+      <Info>El precio más alto del día: <span>{result.HIGHDAY}</span></Info>
+      <Info>El precio más bajo del día: <span>{result.LOWDAY}</span></Info>
+      <Info>Variación últimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></Info>
+      <Info>Última actualización: <span>{result.LASTUPDATE}</span></Info>
+    </QuotationContainer>
   )
 }
 
